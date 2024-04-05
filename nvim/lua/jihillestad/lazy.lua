f472a9ae44c554ca3e590f32d72164f38e953c6e
Jann Idar Hillestad <jihillestad@hotmail.com>
2024-04-06 01:31:20 +0200

    0.0.2 obsidian config updates

--- nvim/lua/jihillestad/lazy.lua
+++ nvim/lua/jihillestad/lazy.lua
@@ -20,17 +20,4 @@ require("lazy").setup({ { import = "jihillestad.plugins" }, { import = "jihilles
     notify = false,
   },
 })
-
--- function SetTermColors()
---   --   vim.g.nightflyTransparent = true
---   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---   vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
---   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
---   vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
---   vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
---   vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
---   vim.api.nvim_set_hl(0, "BufferLineFill", { bg = "none" })
--- end
--- SetTermColors()
-
 vim.g.skip_ts_context_commentstring_module = true
