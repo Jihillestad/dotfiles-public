8a6308d67a8289e96c4c13f856644acd38158436
Jann Idar Hillestad <jihillestad@hotmail.com>
2024-04-21 23:32:04 +0200

    0.1.0 zsh, fzf and bat improvements

--- nvim/lua/jihillestad/plugins/colorscheme.lua
+++ nvim/lua/jihillestad/plugins/colorscheme.lua
@@ -15,24 +15,24 @@ return {
 
       require("tokyonight").setup({
         transparent = false,
-        style = "day",
---        on_colors = function(colors)
---          colors.bg = bg
---          colors.bg_dark = bg_dark
---          colors.bg_float = bg_dark
---          colors.bg_highlight = bg_highlight
---          colors.bg_popup = bg_dark
---          colors.bg_search = bg_search
---          colors.bg_sidebar = bg_dark
---          colors.bg_statusline = bg_dark
---          colors.bg_visual = bg_visual
---          colors.border = border
---          colors.fg = fg
---          colors.fg_dark = fg_dark
---          colors.fg_float = fg
---          colors.fg_gutter = fg_gutter
---          colors.fg_sidebar = fg_dark
---        end, -- End color override
+        style = "night",
+        on_colors = function(colors)
+          colors.bg = bg
+          colors.bg_dark = bg_dark
+          colors.bg_float = bg_dark
+          colors.bg_highlight = bg_highlight
+          colors.bg_popup = bg_dark
+          colors.bg_search = bg_search
+          colors.bg_sidebar = bg_dark
+          colors.bg_statusline = bg_dark
+          colors.bg_visual = bg_visual
+          colors.border = border
+          colors.fg = fg
+          colors.fg_dark = fg_dark
+          colors.fg_float = fg
+          colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
+        end, -- End color override
       })
       -- load the colorscheme here
       vim.cmd([[colorscheme tokyonight]])
