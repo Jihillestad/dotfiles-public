f965bc072b9f06ac3dcd5e309d50ade51888b748
Jann Idar Hillestad <jihillestad@hotmail.com>
2025-09-19 12:26:54 +0200

    Minor fixes in treesitter config

--- nvim/lua/jihillestad/plugins/nvim-treesitter.lua
+++ nvim/lua/jihillestad/plugins/nvim-treesitter.lua
@@ -13,6 +13,8 @@ return {
       -- configure treesitter
       treesitter.setup({ -- enable syntax highlighting
 
+        ignore_install = {},
+
         sync_install = false,
 
         auto_install = true,
@@ -20,9 +22,10 @@ return {
         highlight = {
           enable = true,
         },
+
         -- enable indentation
         indent = { enable = true },
-        --
+
         -- ensure these language parsers are installed
         ensure_installed = {
           "json",
@@ -59,7 +62,13 @@ return {
           },
         },
         -- tell treesitter to use the markdown parser for mdx files
-        -- vim.treesitter.language.register("markdown", "mdx"),
+        vim.treesitter.language.register("markdown", "mdx"),
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
       })
     end,
   },
