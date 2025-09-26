951c166fcda3867dd51935bfa01aa118d42ae194
Jann Idar Hillestad <jihillestad@hotmail.com>
2025-09-26 12:57:18 +0200

    Fixed minor issues

--- nvim/lua/jihillestad/plugins/auto-session.lua
+++ nvim/lua/jihillestad/plugins/auto-session.lua
@@ -1,15 +1,16 @@
 return {
   "rmagatti/auto-session",
   config = function()
-    local auto_session = require("auto-session")
+    -- Fix sessionoptions for auto-session
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
+    local auto_session = require("auto-session")
     auto_session.setup({
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      auto_restore = false, -- Updated from auto_restore_enabled
+      suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" }, -- Updated from auto_session_suppress_dirs
     })
 
     local keymap = vim.keymap
-
     keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
     keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
   end,
